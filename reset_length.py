8256bd148c4937131eb822d7bb59fb8206048655
Sid Naik <sid.naik27@gmail.com>
2018-03-11 10:06:11 -0700

    Delete reset_length.py

--- reset_length.py
+++
@@ -1,9 +0,0 @@
-import bpy
-
-obj = bpy.context.object    
-if obj.type == 'ARMATURE':
-       for bone in obj.pose.bones:
-           for con in bone.constraints:
-               if con.type == 'STRETCH_TO':           
-                   con.rest_length = 0
-             
